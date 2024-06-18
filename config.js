acf4b1c20bf729a5bc55eb3a24ae50252178cc1d
YaoXT0508 <131424001+YaoXT0508-lab@users.noreply.github.com>
2024-06-18 15:36:26 +0800

    Update config.js
    Signed-off-by: YaoXT0508 <131424001+YaoXT0508-lab@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-// YaoXT 2024/06/18  https://Github.com/AJLoveChina
+// YaoXT 2024/06/18  https://yaoxt0508-lab.github.io/LoveTree/
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-        "亲爱的大哥",
-        "没有爱的生命，是没有花的春天",
+        "xxx",
+        "xxxxxxxx",
         "I love you as who you are",
-        "我爱你，如你所是",
+        "xxxxxxxxxx",
     ],
-    // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
     // 分别是 年-月-日T时时:分分:秒秒
     //      温馨警告:
@@ -15,5 +14,5 @@ var config = {
     //          2.T不能丢了
     date: "2011-04-12T00:00:00",
     // 双方的名字
-    names: ["大哥", "宝宝"]
+    names: ["xx", "xx"]
 }
